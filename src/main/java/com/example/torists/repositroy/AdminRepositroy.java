1355b02bc23fec9123bbd090d000b59393591035
xjw <1070922300@qq.com>
2018-07-06 15:51:27 +0800

    修改AdminRepositroy

--- src/main/java/com/example/torists/repositroy/AdminRepositroy.java
+++ src/main/java/com/example/torists/repositroy/AdminRepositroy.java
@@ -1,4 +1,36 @@
 package com.example.torists.repositroy;
 
+import com.example.torists.bean.Admin;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
 public interface AdminRepositroy {
+    /**
+     * 查询全部管理员
+     * @return
+     */
+    List<Admin> findAllAdmin();
+
+    /**
+     * 添加管理员
+     * @param admin
+     * @return
+     */
+    int addAdmin(Admin admin);
+
+    /**
+     * 修改管理员
+     * @param admin
+     * @return
+     */
+    int updateAdmin(Admin admin);
+
+    /**
+     * 删除管理员
+     * @param id
+     * @return
+     */
+    int deleteAdmin(Integer id);
+
 }
